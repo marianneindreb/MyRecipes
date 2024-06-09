38f2abc48596062db09b9e17fefbe41c709248b5
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-06-09 15:54:16 +0200

    initial commit

---
+++ MyRecipes1/MyRecipes1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyRecipes1
+//
+//  Created by Marianne Indrebø on 09/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyRecipes1/MyRecipes1/MyRecipes1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecipes1App.swift
+//  MyRecipes1
+//
+//  Created by Marianne Indrebø on 09/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyRecipes1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
