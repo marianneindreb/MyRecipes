233e5b3dc62ef1889b0f4023ef023db897c5f504
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-06-09 15:59:25 +0200

    feat: recipe-model

---
+++ MyRecipes1/MyRecipes1/Model/RecipeModel.swift
@@ -0,0 +1,39 @@
+import Foundation
+import SwiftData
+
+@Model
+class Recipe: Identifiable {
+    var title: String
+    var shortDescription: String
+    
+    @Relationship(inverse: \Category.recipes)
+    var category: Category?
+    var imageData: Data?
+    var preparationTime: String
+    var directions: String
+    var ingredient: String
+   
+    
+    init(title: String = "", shortDescription: String = "", imageData: Data? = nil, category: Category? = nil, preparationTime: String = "", directions: String = "", ingredient: String = "") {
+        self.title = title
+        self.shortDescription = shortDescription
+        self.imageData = imageData
+        self.category = category
+        self.preparationTime = preparationTime
+        self.directions = directions
+        self.ingredient = ingredient
+    }
+}
+
+@Model
+class Category {
+    @Attribute(.unique)
+    var title: String
+    var image: Data?
+    var recipes: [Recipe]?
+    
+    init(title: String, image: Data? = nil) {
+        self.title = title
+        self.image = image
+    }
+}
