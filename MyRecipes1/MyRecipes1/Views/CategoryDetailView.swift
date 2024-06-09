0ecc47cd1f077eb79f040ead5f40e2e1fcbc8099
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-06-09 21:24:04 +0200

    feat: project-structure (#2)

---
+++ MyRecipes1/MyRecipes1/Views/CategoryDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryDetailView.swift
+//  MyRecipes1
+//
+//  Created by Marianne Indrebø on 09/06/2024.
+//
+
+import SwiftUI
+
+struct CategoryDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryDetailView()
+}
---
+++ MyRecipes1/MyRecipes1/Views/RecipeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailView.swift
+//  MyRecipes1
+//
+//  Created by Marianne Indrebø on 09/06/2024.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeDetailView()
+}
