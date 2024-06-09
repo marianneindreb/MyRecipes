f1118d885a24be104bdb00e4b89ce45e161f1a1d
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-06-09 21:23:19 +0200

    feat: project-structure

---
+++ MyRecipes1/MyRecipes1/Components/CategoryItems.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItems.swift
+//  MyRecipes1
+//
+//  Created by Marianne Indrebø on 09/06/2024.
+//
+
+import SwiftUI
+
+struct CategoryItems: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItems()
+}
