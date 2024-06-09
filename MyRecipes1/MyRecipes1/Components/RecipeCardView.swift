7e36588d23393d891211bf4740d1122a8792fb9b
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-06-09 21:27:22 +0200

    feat:recipe-card

--- MyRecipes1/MyRecipes1/Components/RecipeCardView.swift
+++ MyRecipes1/MyRecipes1/Components/RecipeCardView.swift
@@ -1,18 +1,66 @@
-//
-//  RecipeCardView.swift
-//  MyRecipes1
-//
-//  Created by Marianne Indrebø on 09/06/2024.
-//
-
 import SwiftUI
 
 struct RecipeCardView: View {
+    let recipe: Recipe
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: RecipeDetailView()) {
+            HStack(alignment: .top) {
+                if let imageData = recipe.imageData,
+                   let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 120, height: 140)
+                        .cornerRadius(8)
+                } else {
+                    Image("mat")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 120, height: 140)
+                        .cornerRadius(8)
+                }
+                VStack(alignment: .leading) {
+                    
+                    HStack {
+                        if let category = recipe.category {
+                            Text(category.title)
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        Spacer()
+                        Image(systemName: "heart")
+                            .font(.system(size: 18))
+                            .padding(5)
+                    }
+                    Text(recipe.title)
+                        .font(.headline)
+                        .bold()
+                    
+                    Text(recipe.shortDescription)
+                        .font(.caption)
+                    
+                    Spacer()
+                    
+                    if !recipe.preparationTime.isEmpty {
+                        HStack {
+                            Image(systemName: "clock")
+                                .foregroundStyle(.yellow)
+                            Text(recipe.preparationTime)
+                                .font(.caption)
+                        }
+                        .padding(.bottom, 5)
+                    }
+                    
+                }
+                .padding(.vertical, 5)
+            }
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+            .padding(.vertical, 1)
+        }
+        .buttonStyle(.plain)
     }
-}
-
-#Preview {
-    RecipeCardView()
 }
